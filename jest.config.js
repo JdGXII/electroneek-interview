6bebca66ce8c3ecc22fcc119bef52ac8c154b80c
josevaltech <jose.alzaibar@valtech.com>
2023-06-06 12:32:09 -0500

    added reporter

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,12 @@ module.exports = {
       '^.+\\.ts?$': 'babel-jest',
     },
     testTimeout: 10000, //I was having internet issues when developing the tests so had to pump this up in the meantime
+    reporters: [
+      'default',
+      ['jest-html-reporter', {
+        pageTitle: 'Test Report',
+        outputPath: './test-report.html',
+      }],
+    ],
   };
   
