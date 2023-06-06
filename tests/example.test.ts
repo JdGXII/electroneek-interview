bb463f9793a87a30ecd1561c52bd4f7c59cc2571
josevaltech <jose.alzaibar@valtech.com>
2023-06-05 20:06:02 -0500

    added rest of tests to file

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -37,4 +37,42 @@ describe('E2E Tests', () => {
     const inventoryContainer = await page.isVisible('#inventory_container');
     expect(inventoryContainer).toBe(true);
   });
+
+  test('Invalid Login - Verify Error Message', async () => {
+    await page.goto('https://www.saucedemo.com/');
+  
+    // Enter invalid username and password
+    await page.fill('#user-name', 'invaliduser');
+    await page.fill('#password', 'invalidpassword');
+  
+    // Click the login button
+    await page.click('#login-button');
+  
+    // Check for the error message
+    const errorMessage = await page.textContent('h3[data-test="error"]');
+    expect(errorMessage).toBe('Epic sadface: Username and password do not match any user in this service');
+  });
+
+  test('Cart Functionality - Verify Add and Remove Items', async () => {
+    await page.goto('https://www.saucedemo.com/');
+  
+    // Login with valid credentials
+    await page.fill('#user-name', 'standard_user');
+    await page.fill('#password', 'secret_sauce');
+    await page.click('#login-button');
+  
+    // Add item to cart
+    await page.click('.btn_inventory');
+    await page.click('.shopping_cart_link');
+  
+    // Verify item in the cart
+    const cartItems = await page.$$('.cart_item');
+    expect(cartItems.length).toBe(1);
+  
+    // Remove item from cart
+    await page.click('.cart_item .btn_secondary');
+    const emptyCartMessage = await page.textContent('.cart_list');
+    expect(emptyCartMessage).toContain('Your cart is empty');
+  });
+  
 });
